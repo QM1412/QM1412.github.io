bbaeaccc64927d0c22ff13e1b9bd21558bc453b4
QM1412 <13002094085@163.com>
2023-02-15 11:01:30 +0800

    Site updated: 2023-02-15 11:01:30

--- js/readPercent.js
+++ js/readPercent.js
@@ -1,17 +1,1 @@
-window.onscroll = percent;// ִ�к���
-// ҳ��ٷֱ�
-function percent() {
-    let a = document.documentElement.scrollTop || window.pageYOffset, // ��ȥ�߶�
-        b = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.body.clientHeight, document.documentElement.clientHeight) - document.documentElement.clientHeight, // ������ҳ�߶�
-        result = Math.round(a / b * 100), // ����ٷֱ�
-        up = document.querySelector("#go-up") // ��ȡ��ť
-
-    if (result <= 95) {
-        up.childNodes[0].style.display = 'none'
-        up.childNodes[1].style.display = 'block'
-        up.childNodes[1].innerHTML = result;
-    } else {
-        up.childNodes[1].style.display = 'none'
-        up.childNodes[0].style.display = 'block'
-    }
-}
+function percent(){let e=document.documentElement.scrollTop||window.pageYOffset,t=Math.max(document.body.scrollHeight,document.documentElement.scrollHeight,document.body.offsetHeight,document.documentElement.offsetHeight,document.body.clientHeight,document.documentElement.clientHeight)-document.documentElement.clientHeight,o=Math.round(e/t*100),n=document.querySelector("#go-up");o<=95?(n.childNodes[0].style.display="none",n.childNodes[1].style.display="block",n.childNodes[1].innerHTML=o):(n.childNodes[1].style.display="none",n.childNodes[0].style.display="block")}window.onscroll=percent;
